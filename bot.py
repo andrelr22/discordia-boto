efc7ad33b5992b87c3719c4439dda83cd6d13f60
andrelr22 <32082449+andrelr22@users.noreply.github.com>
2018-02-22 18:18:02 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,53 @@
+import json
+import discord
+from discord.ext import commands
+
+
+arq_json=open("values.json","r")
+dados_json=json.load(arq_json)
+token=dados_json['token']
+
+print(token)
+
+
+bot = commands.Bot(command_prefix='$', description='A retarded bot')
+
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def teste(ctx, a: int, b: int):
+    await ctx.send("This is a test")
+
+
+async def info(ctx):
+    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
+    
+    # give info about you here
+    embed.add_field(name="Author", value="<YOUR-USERNAME>")
+    
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+
+    await ctx.send(embed=embed)
+
+async def help(ctx):
+    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
+
+    embed.add_field(name="$add X Y", value="Gives the addition of **X** and **Y**", inline=False)
+    embed.add_field(name="$multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
+    embed.add_field(name="$greet", value="Gives a nice greet message", inline=False)
+    embed.add_field(name="$cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+    embed.add_field(name="$info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name="$help", value="Gives this message", inline=False)
+
+    await ctx.send(embed=embed)
+
+bot.run(token)
